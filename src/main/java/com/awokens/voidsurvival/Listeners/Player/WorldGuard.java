3675a4b7e24c539bf27fcefdb0d967f31ebdb95d
Awokens <35590883+Awokens@users.noreply.github.com>
2024-04-03 22:16:24 -0700

    Prevent Lava and Water from forming blocks (e.g. Cobblestone or Stone)

--- src/main/java/com/awokens/voidsurvival/Listeners/Player/WorldGuard.java
+++ src/main/java/com/awokens/voidsurvival/Listeners/Player/WorldGuard.java
@@ -1,14 +1,12 @@
 package com.awokens.voidsurvival.Listeners.Player;
 
 import com.awokens.voidsurvival.Manager.SpawnPointManager;
-import org.bukkit.GameMode;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockFertilizeEvent;
+import org.bukkit.event.block.BlockFormEvent;
 import org.bukkit.event.block.BlockPhysicsEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -46,6 +44,7 @@ public class WorldGuard implements Listener {
 
     @EventHandler
     public void physics(BlockPhysicsEvent event) {
+
         if (!inProtectedRegion(event.getBlock().getLocation())) return;
 
         if (!event.getBlock().isLiquid()) return;
@@ -54,6 +53,31 @@ public class WorldGuard implements Listener {
         event.getSourceBlock().setType(Material.AIR);
 
     }
+
+    @EventHandler
+    public void form(BlockFormEvent event) {
+        Location position = event.getBlock().getLocation();
+
+
+        World world = event.getBlock().getWorld();
+        WorldBorder border = world.getWorldBorder();
+
+        Location center = border.getCenter().clone().toCenterLocation();
+
+        int size = (int) border.getSize();
+        int radius = (size / 2) + 1;
+
+        Location cornerA = center.clone().subtract(radius, 0, radius);
+        cornerA.setY(world.getMinHeight());
+        Location cornerB = center.clone().add(radius, 0, radius);
+        cornerB.setY(world.getMaxHeight());
+
+        if (inBound(position, cornerA, cornerB )) return;
+
+        event.setCancelled(true);
+
+    }
+
 
     @EventHandler
     public void fertilize(BlockFertilizeEvent event) {
